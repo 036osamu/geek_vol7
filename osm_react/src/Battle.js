610e081b9c110b620eb688f9df959cace1293c2f
Hiroki Ishimaru <>
2021-07-29 22:57:03 +0900

    react firebase touch

--- osm_react/src/Battle.js
+++ osm_react/src/Battle.js
@@ -7,8 +7,10 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
+let hp = 0;
+
 function Battle() {
-    export const app = firebase.initializeApp({
+    const firebaseConfig={
         apiKey: process.env.REACT_APP_FIREBASE_KEY,
         authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
         projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -16,12 +18,16 @@ function Battle() {
         messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
         appId: process.env.REACT_APP_FIREBASE_APP_ID,
         measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-    });
+    };
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
     let db = firebase.firestore();
     let docRef = db.collection("data").doc("90TwVL13wTuPpmCgw24C")
         .onSnapshot((doc) => {
             console.log("Current data: ", doc.data());
-            let hp = doc.data();
+            hp = doc.data();
+
         });
 
     return (
