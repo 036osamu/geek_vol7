4e7016abe86079051de445554b6d2c65623e16d1
Hiroki Ishimaru <>
2021-07-29 20:47:42 +0900

    react

---
+++ osm_react/src/Battle.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
+
+function Battle() {
+    return (
+        <div className="Battle">
+            <Button variant="primary">Primary</Button>{/* ? */}
+        </div>
+    );
+}
+export default Battle;
